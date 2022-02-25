b57f4dd9e928ea0a0374454c0e0c08ee93a0371b
ivanbogaeb <ivanbogdasaebersold@live.com.ar>
2022-02-24 23:04:52 -0300

    Update execute module
    Module error handling added.

--- components/steroid/routes/controller/execute.js
+++ components/steroid/routes/controller/execute.js
@@ -1,7 +1,12 @@
 const handler = require('../../modules/execute');
 
 const execute = async (req, res) => {
-    let data = await handler(req.query.executable, req.query.parameters);
+    let data;
+    if (!!req.query.executable){
+        data = await handler(req.query.executable, req.query.parameters);
+    } else {
+        data = {error: "Executable error, please add 'executable' and 'parameters' on URL query parameters."};
+    }
     return res.send(data);
 };
 module.exports = execute;
