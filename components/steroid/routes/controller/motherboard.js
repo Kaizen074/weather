572f9638ee6238e6ecde0907469f727c060597e1
ivanbogaeb <ivanbogdasaebersold@live.com.ar>
2022-08-27 00:20:48 -0300

    Redundant functions removal

--- components/steroid/routes/controller/motherboard.js
+++ components/steroid/routes/controller/motherboard.js
@@ -2,23 +2,8 @@ const handler = require('../../modules/motherboard');
 
 const motherboard = async (req, res) => {
     let data;
-    switch(req.query.function){
-        case 'system':
-            data = await handler.system();
-           return  res.send(data);
-        case 'bios':
-            data = await handler.bios();
-            return res.send(data);
-        case 'baseboard':
-            data = await handler.baseboard();
-            return res.send(data);
-        case 'chassis':
-            data = await handler.chassis();
-            return res.send(data);
-        default:
-            res.send({error: "Function error, please try '?function=system', '?function=bios', '?function=baseboard', '?function=chassis' on URL query parameters."})
-            break;
-    }
+    data = await handler.baseboard();
+    return res.send(data);
 };
 
 module.exports = motherboard;
