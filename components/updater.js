58bc80a345e9b99bbb5165a5f5308fcaa2e77a29
Iván Bogdas Aebersold <93013351+ivanbogaeb@users.noreply.github.com>
2022-09-20 12:31:54 -0300

    Private token removal
    Private token has been removed since it's not required anymore.

--- components/updater.js
+++ components/updater.js
@@ -20,7 +20,7 @@ const updater = async (app, path, Sakura) => {
         let sakura = new Sakura({
             gitUsername: 'ivanbogaeb',
             gitRepository: 'steroid-app',
-            privateToken: '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',
+            privateToken: '',
             installationDirectory: path.join(__dirname, '../'),
             hasExecutable: true,
             executableName: 'steroid.exe',
